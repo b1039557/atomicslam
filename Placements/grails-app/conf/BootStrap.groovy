5fdc538e8afbeb5a265c4b93bd215b1f5893ed64
umair alam <umair_alam18@hotmail.com>
2012-05-06 15:15:26 +0100

    added test data via scaffolding for all domain and also added default data to each domain type via the bootstrap class

--- Placements/grails-app/conf/BootStrap.groovy
+++ Placements/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,23 @@
+import placements.Student
+import placements.PlacementOppurtunity
+import placements.Status
+import placements.Application 
+
+
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = { servletContext -> 
+
+    def  studentumair = new Student(name:'umair',courseCode:'T500', notes:'talanted' ,applications:'five').save();
+
+    def  applicationmoh = new Application(student:'muhammad', placement:'technical' ,status:'open' ,timestamp:'12/03/2011').save();   
+
+    def  statuslol = new Status(code:'123jkbd343',description:'join programme').save();
+
+     def Placeopp = new PlacementOppurtunity(jobTitle:'software engineer', companyName:'microsoft', applications:'five', status:'extended').save();    
+   
+   
+
     }
     def destroy = {
     }
